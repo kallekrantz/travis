4903cf4e88b987bd53b3653cdcea42426d92ede5
Karl Johan Krantz <schwomp@gmail.com>
2013-09-14 19:39:14 +0200

    Now supporting a county

--- server/get.py
+++ server/get.py
@@ -1,17 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import json
+import sys
 def main():
     initPage = 0
     page = initPage
-    url = "http://api.apitekt.se/transportstyrelsen/olyckor-2003-2012/list.json?kommun=Link%%C3%%B6ping&page=%s"
+    url = "http://api.apitekt.se/transportstyrelsen/olyckor-2003-2012/list.json?lan=Östergötlands%20län&page=".decode('utf-8')
     result = []
+    temp = requests.get(url + str(page)).json()
+
     while True:
-        temp = requests.get(url % page).json()
+        sys.stderr.write("Printing page: " + str(page) + "\n")
+        temp = requests.get(url + str(page)).json()
         page+=1
         if len(temp['rows']) == 0:
             break
         result += temp['rows']
-
+        
     print json.dumps(result);
         
 if __name__ == "__main__":
